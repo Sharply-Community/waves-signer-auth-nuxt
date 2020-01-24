bf5182e8eeb22b784709ae4b08327bb8caf53ecb
Kelvin Omereshone <kelvinomereshone@gmail.com>
2020-01-24 11:31:35 +0100

    stylint configurations

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,14 @@
     <div>
       <logo />
       <h1 class="title">
-        waves-signer-auth-nuxt
+        Waves Signer
       </h1>
       <h2 class="subtitle">
         Demo project for Waves Signer Authentication
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
+        <a href="#" target="_blank" class="button--green">
+          Login
         </a>
       </div>
     </div>
